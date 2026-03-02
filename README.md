54736b568383cb948ceb8e54d9c65f190a3627ca
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2026-03-02 19:55:48 +0100

    unbound

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# nixcfg_home_server
 
+<pre>
+  Title: Home Server
+  Author: Momfer de Mol
+  Status: Active
+  Created: 02-03-2026
+</pre>
+
+# Home Server
+
+## LXC
+
+- Unbound
+
+# References
+
+- [Proxmox LXC Containers](https://nixos.wiki/wiki/Proxmox_Linux_Container#Running_NixOS_as_a_ProxmoxVE_LXC_container)
---
+++ lxc/unbound/configuration.nix
@@ -0,0 +1,155 @@
+{ config, modulesPath, pkgs, lib, ... }:
+
+{
+  imports = [
+    "${modulesPath}/virtualisation/proxmox-lxc.nix"
+  ];
+
+  nix.settings = {
+    sandbox = false;
+  };
+
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+
+  services.fstrim = {
+    enabled = false;
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+    dates = "Monday 04:00 UTC";
+    rebootWindow = {
+      lower = "04:00";
+      upper = "06:00";
+    };
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "Monday 07:00 UTC";
+    options = "--delete-older-than 7d";
+  };
+
+  # Run garbage collection whenever there is less than 500MB free space left
+  nix.extraOptions = ''
+    min-free = ${toString (500 * 1024 * 1024)}
+  '';
+
+  # clean up system logs old then 1 month
+  systemd = {
+    services.clear-log = {
+      description = "clean 30+ old logs every week";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=30d";
+      };
+    };
+    timers.clear-log = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "clear-log.service" ];
+      timerConfig.OnCalendar = "weekly UTC";
+    };
+  };
+
+  networking = {
+    hostName = "unbound";
+    useDHCP = false;
+    interfaces.eth0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.10.10";
+          prefixLength = 32;
+        }
+      ];
+    };
+
+    defaultGateway = {
+      address = "192.168.10.1";
+      interface = "eth0";
+    };
+
+    networkmanager = {
+      enable = true;
+    };
+
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [ 53 ];
+      allowedTCPPorts = [ 53 ];
+    };
+  };
+
+  time.timeZone = "Europe/Amsterdam";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users = {
+    users = {
+      unbound = {
+        home = "/var/lib/unbound";
+        createHome = true;
+        isSystemUser = true;
+        group = "unbound";
+      };
+    };
+
+    groups = {
+      unbound = {
+        members = [ "unbound" ];
+      };
+    };
+  };
+
+  programs.bash = {
+    loginShellInit = ''
+      ${pkgs.fastfetch}/bin/fastfetch
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    fastfetch
+    dig
+    unbound
+  ];
+
+  services.unbound = {
+    enable = true;
+    user = "unbound";
+    group = "unbound";
+    settings = {
+      server = {
+        verbosity = 1;
+        auto-trust-anchor-file = "/var/lib/unbound/root.key";
+        qname-minimisation = true;
+        interface = "0.0.0.0";
+        access-control = "192.168.0.0/16 allow";
+        private-domain = "lan.d35c.net";
+        local-zone = "\"lan.d35c.net.\" redirect";
+        local-data = [
+          "\"lan.d35c.net.\tIN A 192.168.10.11\""
+        ];
+      };
+
+      forward-zone = [
+        {
+          name = ".";
+          forward-addr = [
+            "1.1.1.1"
+            "9.9.9.9"
+          ];
+        }
+      ];
+
+      remote-control = {
+        control-enable = false;
+      };
+    };
+    enableRootTrustAnchor = true;
+  };
+
+  system.stateVersion = "25.05";
+}
